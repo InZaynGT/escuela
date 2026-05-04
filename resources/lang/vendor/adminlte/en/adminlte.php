a6889370129cdca26224368c3abd1d54137fa842
InZaynGT <carlosperez280202@gmail.com>
2026-05-04 17:39:05 -0600

    commit prueba automatica

--- resources/lang/vendor/adminlte/en/adminlte.php
+++ resources/lang/vendor/adminlte/en/adminlte.php
@@ -14,7 +14,7 @@ return [
     'sign_in' => 'Sign In',
     'log_out' => 'Log Out',
     'toggle_navigation' => 'Toggle navigation',
-    'login_message' => 'Inicia sesión',
+    'login_message' => 'Iniciar sesión',
     'register_message' => 'Register a new membership',
     'password_reset_message' => 'Reset Password',
     'reset_password' => 'Reset Password',
